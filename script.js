b53bb88b3ab7c8151b3991df12ab117f8386641d
AlphatTrons <pradyun4kumar4sinha4@gmail.com>
2024-10-06 20:42:58 +0400

    Proposal.html

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("yesButton").addEventListener("click", function() {
+    window.location.href = "yes.html";
+  });
+  
+  document.getElementById("noButton").addEventListener("click", function() {
+    window.location.href = "no.html";
+  });
+  
